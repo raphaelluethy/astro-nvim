c71f11af05252e7819477ed672ebd9869c8dc815
Raphael Lüthy <raphael.luethy@fhnw.ch>
2025-02-18 19:20:24 +0100

    i am batman

---
+++ lua/community.lua
@@ -0,0 +1,24 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.poimandres-nvim" },
+  -- { import = "astrocommunity.completion.codeium-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
+  { import = "astrocommunity.recipes.disable-tabline" },
+  -- { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
+  -- import/override with your plugins folder
+}
---
+++ lua/plugins/astrocore.lua
@@ -0,0 +1,90 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
+-- Configuration documentation can be found with `:h astrocore`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    -- Configure core features of AstroNvim
+    features = {
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        wrap = false, -- sets vim.opt.wrap
+        list = true, -- sets vim.opt.list
+        tabstop = 4, -- sets vim.opt.tabstop to 4
+        shiftwidth = 4, -- sets vim.opt.shiftwidth to 4
+        textwidth = 80, -- sets vim.opt.textwidth to 80
+        linebreak = true, -- sets vim.opt.linebreak
+        colorcolumn = "80",
+      },
+      g = { -- vim.g.<key>
+        -- configure global vim variables (vim.g)
+        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+        -- This can be found in the `lua/lazy_setup.lua` file
+      },
+    },
+    -- Mappings can be configured through AstroCore as well.
+    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
+
+        -- navigate buffer tabs
+        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+        -- mappings seen under group name "Buffer"
+        ["<Leader>bd"] = {
+          function()
+            require("astroui.status.heirline").buffer_picker(
+              function(bufnr) require("astrocore.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Close buffer from tabline",
+        },
+
+        ["<Esc>"] = { ":nohlsearch<CR>", desc = "Clear search" },
+        ["gh"] = { vim.diagnostic.open_float, desc = "Show diagnostics" },
+        ["<C-h>"] = { "<C-w><C-h>", desc = "Move focus to the left window" },
+        ["<C-l>"] = { "<C-w><C-l>", desc = "Move focus to the right window" },
+        ["<C-j>"] = { "<C-w><C-j>", desc = "Move focus to the lower window" },
+        ["<C-k>"] = { "<C-w><C-k>", desc = "Move focus to the upper window" },
+        ["<leader>sv"] = { "<CMD>vsplit<CR>", desc = "[S]plit [V]ertically" },
+
+        ["c"] = { '"_c', desc = "Change to blackhole register" },
+        ["C"] = { '"_C', desc = "Change to blackhole register" },
+      },
+      v = {
+        ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
+        ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
+        ["<"] = { "<gv", desc = "Reselect after indent" },
+        [">"] = { ">gv", desc = "Reselect after indent" },
+        ["c"] = { '"_c', desc = "Change to blackhole register" },
+        ["C"] = { '"_C', desc = "Change to blackhole register" },
+      },
+    },
+  },
+}
